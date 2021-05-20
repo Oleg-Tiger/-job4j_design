709af11627151ffaa28d5c16526007903179e66e
Oleg-Tiger <free_person@inbox.ru>
2021-05-20 19:56:40 +0300

    Почта. [#184458]

--- src/main/java/ru/job4j/collection/mail/Mail.java
+++ src/main/java/ru/job4j/collection/mail/Mail.java
@@ -10,38 +10,40 @@ public class Mail {
      * множество электронных адресов почт. Если у разных пользователей, есть одинаковые
      * адреса почт, то они объединяются, считается, что это один и тот же полоьзователь.
      * Ему присваивается имя одного из обьъединённых пользователей.
-     * В Данном методе есть локальная переменная merge. Это карта, в которую записываются
-     * в качестве ключа и значения два объединённых пользователя. Ключ - пользователь,
-     * который не занесён в результирующую коллекцию, значение - который занесён.
-     * Делается это для того, чтобы в случае необходимости объединения третьего пользователя
-     * с пользователем-ключом мы объеденили его сразу с пользователем-значением.
+     * В Данном методе в качестве промежуточного результата используется карта.
+     * В неё записывается в качестве ключа пользователь, множество которого мы перенесли(он
+     * не будет фигурировать в финальном списке), а в качестве значения - пользователь,
+     * в множество которого мы его перенесли(Он будет в финальном списке).
+     * То есть, пара ключ-значение представляет из себя пару двух объединённых пользователей.
+     * Либо, если добавленный пользователь ни с кем не объединялся (или к нему скопированы значения
+     * другого, а он сам будет в результирющем списке), то ключ и значение будут равны.
+     * Делается это для того, чтобы в случае необходимости объединения третьего пользователя с
+     * пользователем - ключом(который уже с кем-то объединён), мы перенесли его множество почт сразу
+     * в пользователя-значение(который будет в результирующем списке).
+     * Затем из карты извлекаются значения в множество, чтобы избежать повторов, и возвращается
+     * список на основании этого множества.
      * @param list список пользователей
      * @return новый списаок пользователей, в котором пользователи, имеющие хотя бы одно
      * совпадение по адресам электронных почт, объединены
      */
     public List<User> mergeUsers(List<User> list) {
         Map<Email, List<User>> map = usersForEachMail(list);
-        Set<User> rsl = new HashSet<>();
-        Map<User, User> merge = new HashMap<>();
-      for (Map.Entry<Email, List<User>> pairs : map.entrySet()) {
-          List<User> users = pairs.getValue();
-          User firstInList = users.get(0);
-          if (!merge.containsKey(firstInList)) {
-              rsl.add(firstInList);
-              for (int i = 1; i < users.size(); i++) {
-                  firstInList.getMailSet().addAll(users.get(i).getMailSet());
-                  merge.put(users.get(i), firstInList);
-              }
-          } else {
-              for (int i = 1; i < users.size(); i++) {
-                  merge.get(firstInList).getMailSet().addAll(users.get(i).getMailSet());
-                  merge.put(users.get(i), merge.get(firstInList));
-              }
-
-          }
-      }
-        return new ArrayList<>(rsl);
-      }
+        Map<User, User> rsl = new HashMap<>();
+        for (Map.Entry<Email, List<User>> pairs : map.entrySet()) {
+            List<User> users = pairs.getValue();
+            User firstInList = users.get(0);
+            if (!rsl.containsKey(firstInList)) {
+                rsl.put(firstInList, firstInList);
+            } else {
+                firstInList = rsl.get(firstInList);
+            }
+            for (int i = 1; i < users.size(); i++) {
+                firstInList.getMailSet().addAll(users.get(i).getMailSet());
+                rsl.put(users.get(i), firstInList);
+            }
+        }
+        return new ArrayList<>(new HashSet<>(rsl.values()));
+    }
 
     /**
      * Этот метод принимает на вход список пользователей, у которых есть в качестве
@@ -70,8 +72,6 @@ public class Mail {
         }
         return rsl;
     }
-
-
 }
 
 
