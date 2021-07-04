2f1e7ddab7d7c103c9640035b84352f99434f515
Oleg-Tiger <free_person@inbox.ru>
2021-07-05 00:02:15 +0300

    1. Схема прав пользователи и роли. [#1093]

---
+++ scripts/insert.sql
@@ -0,0 +1,7 @@
+insert into category (name) values('Ошибка авторизации');
+insert into category (name) values('Получение логина и пароля');
+insert into category (name) values('Ошибка подключения');
+
+insert into state (name) values('Принята');
+insert into state (name) values('На рассмотрении');
+insert into state (name) values('Решена');
